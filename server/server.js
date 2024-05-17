e52239b45d090815c618d89f3202a284f9758063
l3vi-Ackerman <l3vi3211@gmail.com>
2024-05-17 08:21:05 +0545

    server.js file uploaded

--- server/server.js
+++ server/server.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const app = express();
+const cors = require('cors')
 const router = require("./Routes/router");
 require("dotenv").config({ path: "./config.env" });
 const connectDB = require("./Database/db-connection");
 
+const corsOptions = {
+  origin: "http://localhost:3000",
+  methods:"GET,POST,PUT,DELETE,PATCH,HEAD",
+  credentials:true
+}
 app.use(express.json());
+app.use(cors(corsOptions))
 app.use("/api", router);
 
 const port = process.env.PORT;
